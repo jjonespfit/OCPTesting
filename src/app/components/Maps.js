523ab4194b4d6b6a5f3f4575a1fb2c88ccebe135
jjonespfit <131902552+jjonespfit@users.noreply.github.com>
2024-07-02 22:06:05 -0500

    Update Maps.js

--- src/app/components/Maps.js
+++ src/app/components/Maps.js
@@ -1,40 +1,36 @@
 import React from "react";
-import { GoogleMap, Marker, Circle, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, Marker, Circle } from "@react-google-maps/api";
 
-const Maps = ({ center, stores, googleMapsApiKey }) => {
+const Maps = ({ center, stores }) => {
   const containerStyle = {
     width: "80%",
     height: "400px",
   };
-
   const defaultCenter = {
     lat: center.latitude,
     lng: center.longitude,
   };
-
   // 10 miles in meters
   const radiusInMeters = 5 * 1609.34;
 
   return (
-    <LoadScript googleMapsApiKey={googleMapsApiKey}>
-      <GoogleMap mapContainerStyle={containerStyle} center={defaultCenter} zoom={12}>
-        <Marker position={defaultCenter} label="You" />
-        {stores.map((store, index) => (
-          <Marker key={index} position={{ lat: store.latitude, lng: store.longitude }} title={store.name} />
-        ))}
-        <Circle
-          center={defaultCenter}
-          radius={radiusInMeters}
-          options={{
-            fillColor: "lightblue",
-            fillOpacity: 0.2,
-            strokeColor: "blue",
-            strokeOpacity: 0.5,
-            strokeWeight: 1,
-          }}
-        />
-      </GoogleMap>
-    </LoadScript>
+    <GoogleMap mapContainerStyle={containerStyle} center={defaultCenter} zoom={12}>
+      <Marker position={defaultCenter} label="You" />
+      {stores.map((store, index) => (
+        <Marker key={index} position={{ lat: store.latitude, lng: store.longitude }} title={store.name} />
+      ))}
+      <Circle
+        center={defaultCenter}
+        radius={radiusInMeters}
+        options={{
+          fillColor: "lightblue",
+          fillOpacity: 0.2,
+          strokeColor: "blue",
+          strokeOpacity: 0.5,
+          strokeWeight: 1,
+        }}
+      />
+    </GoogleMap>
   );
 };
 
